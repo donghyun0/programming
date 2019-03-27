f46be3dacc3beaa546d67ef94a12a286bbad0e00
dong <dlehdgus0426@naver.com>
2019-03-27 19:02:06 +0900

    init cimmit

---
+++ src/MyFile.kt
@@ -0,0 +1,14 @@
+package com.example.ch3.one
+
+import java.util.*
+
+class T1_User{
+    val name = "kkang"
+
+    fun sayHello(){
+        val date = Date(){
+            println("date : " + date)
+            println("Hello!! " + name)
+        }
+    }
+}
